7d9ccbadb46f4663994593cb955ecce79ded9a60
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-07 12:26:06 -0300

    Add function to delete associated reviews to a document

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+
+const Review = require('./review');
+
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -13,6 +16,17 @@ const CampgroundSchema = new Schema({
       ref: 'Review',
     },
   ],
+});
+
+// Remove all reviews associated with a document (a camp in this case)
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
