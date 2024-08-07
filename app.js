c2aaef690bded32f96e96917444355300901fff9
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-07 11:00:43 -0300

    Implement review api/form

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
-const { campgroundSchema } = require('./schemas.js');
+const Review = require('./models/review');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const review = require('./models/review');
 
 const app = express();
 
@@ -36,6 +38,16 @@ app.use(methodOverride('_method'));
 // Data validation
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(',');
     throw new ExpressError(msg, 400);
@@ -104,6 +116,19 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+  }),
+);
+
+app.post(
+  '/campgrounds/:id/reviews',
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   }),
 );
 
