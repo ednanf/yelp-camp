cbf36a03f426884fdff716a42f40fd82a496464f
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-19 14:03:54 -0300

    Style details page

--- models/campground.js
+++ models/campground.js
@@ -3,24 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  author: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  title: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  price: {
-    type: String,
-  },
+  title: String,
+  image: String,
+  price: Number,
+  description: String,
+  location: String,
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
