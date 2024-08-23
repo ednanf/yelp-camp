56a65fb0816559a7f5709febdf577e765cff6510
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-23 10:47:06 -0300

    Finish implementing delete function to comments, including when campground is deleted

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const { campgroundSchema } = require('../schemas');
+
+const Review = require('./review');
+
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -15,4 +19,16 @@ const CampgroundSchema = new Schema({
   ],
 });
 
+// Middleware
+
+// Post middleware - will run **after** the condition is met
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 module.exports = mongoose.model('Campground', CampgroundSchema);
