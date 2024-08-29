60b8f4f7821de54bc6db3af353abf0a4d8dec722
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-29 12:49:05 -0300

    Add function to make user logged in after creating an account

--- routes/users.js
+++ routes/users.js
@@ -17,8 +17,13 @@ router.post(
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash('success', 'Welcome to Yelp Camp!');
-      res.redirect('/campgrounds');
+
+      // This login() comes from passport and ensures the user will be logged in after signing up
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+      });
     } catch (e) {
       req.flash('error', e.message);
       res.redirect('/register');
