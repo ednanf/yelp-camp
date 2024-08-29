1dc166210a8d4cc115ab2add3176ef3c0673d4ec
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-08-29 13:06:17 -0300

    Fix redirection after login to the same page as before

--- middleware.js
+++ middleware.js
@@ -1,8 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
   // The method isAuthenticated is added by passport to the request
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
     req.flash('error', 'You must be logged in to perform this action!');
     return res.redirect('/login');
+  }
+  next();
+};
+
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
   }
   next();
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 
 const catchAsync = require('../utils/catchAsync');
+const { storeReturnTo } = require('../middleware');
 
 const User = require('../models/user');
 
@@ -35,10 +36,16 @@ router.get('/login', (req, res) => {
   res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  req.flash('success', 'Welcome back!');
-  res.redirect('/campgrounds');
-});
+router.post(
+  '/login',
+  storeReturnTo,
+  passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+  (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl);
+  },
+);
 
 router.get('/logout', (req, res, next) => {
   req.logout(function (err) {
