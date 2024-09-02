f6f2ea77567b1d01a981aba0b2a0a03be0d65606
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-09-02 10:48:55 -0300

    Refactor login routes to controllers

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -10,11 +10,13 @@ const campgroundControllers = require('../controllers/campgrounds');
 router.get('/', catchAsync(campgroundControllers.index));
 
 router.get('/new', isLoggedIn, campgroundControllers.renderNewForm);
+
 router.post('/', isLoggedIn, validateCampground, catchAsync(campgroundControllers.createCampground));
 
 router.get('/:id', catchAsync(campgroundControllers.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgroundControllers.renderEditForm));
+
 router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundControllers.updateCampground));
 
 router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgroundControllers.deleteCampground));
--- routes/users.js
+++ routes/users.js
@@ -5,56 +5,21 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const { storeReturnTo } = require('../middleware');
 
-const User = require('../models/user');
+const userController = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-});
+router.get('/register', userController.renderRegisterForm);
 
-router.post(
-  '/register',
-  catchAsync(async (req, res) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
+router.post('/register', catchAsync(userController.register));
 
-      // This login() comes from passport and ensures the user will be logged in after signing up
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
-      });
-    } catch (e) {
-      req.flash('error', e.message);
-      res.redirect('/register');
-    }
-  }),
-);
-
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
+router.get('/login', userController.renderLogin);
 
 router.post(
   '/login',
   storeReturnTo,
   passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
-  (req, res) => {
-    req.flash('success', 'Welcome back!');
-    const redirectUrl = res.locals.returnTo || '/campgrounds';
-    res.redirect(redirectUrl);
-  },
+  userController.login,
 );
 
-router.get('/logout', (req, res, next) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    req.flash('success', 'Goodbye!');
-    res.redirect('/campgrounds');
-  });
-});
+router.get('/logout', userController.logout);
 
 module.exports = router;
